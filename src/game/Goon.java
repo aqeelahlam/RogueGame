45e41e3680432e100883c22ab4a0b791b9a974c2
Aqeel Ahlam <aahl0002@student.monash.edu>
2019-04-19 11:22:10 +0800

    Skeleton(Methods not implemented yet)

--- src/game/Goon.java
+++ src/game/Goon.java
@@ -7,8 +7,10 @@ import java.util.List;
 
 //MAKE CHANGES TO ADAPT: STILL IMPLEMENTING
 public class Goon extends Actor {
+
+    // Grunts have 100(twice of Grunt) hitpoints and are always represented with a G
     public Goon(String name, Actor player) {
-        super(name, 'G', 5, 50);
+        super(name, 'G', 5, 100);
         addBehaviour(new FollowBehaviour(player));
     }
 
