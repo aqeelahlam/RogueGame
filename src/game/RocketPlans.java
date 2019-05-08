e94d290359c0a8fde846f6bc38c60aafbd035313
ygan0007 <ygan0007@student.monash.edu>
2019-05-09 06:30:06 +0800

    RocketPlans Completed.

--- src/game/RocketPlans.java
+++ src/game/RocketPlans.java
@@ -1,4 +1,19 @@
 package game;
 
-public class RocketPlans {
+import edu.monash.fit2099.engine.DropItemAction;
+import edu.monash.fit2099.engine.Item;
+
+public class RocketPlans extends Item {
+
+    public RocketPlans(){
+        super("Rocket Plans", 'Ø');
+    }
+
+    public static RocketPlans newRocketPlansInstance() {
+
+        RocketPlans newRocketPlans = new RocketPlans();
+        newRocketPlans.allowableActions.clear();
+        newRocketPlans.allowableActions.add(new DropItemAction());
+        return newRocketPlans;
+    }
 }
