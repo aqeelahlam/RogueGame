5e86b6a1c6f3f2d058b00d341c81f4de3f9887f6
ygan0007 <ygan0007@student.monash.edu>
2019-05-09 06:24:15 +0800

    RocketBody Completed.

--- src/game/RocketBody.java
+++ src/game/RocketBody.java
@@ -1,4 +1,22 @@
 package game;
 
-public class RocketBody {
+import edu.monash.fit2099.engine.DropItemAction;
+import edu.monash.fit2099.engine.Item;
+
+public class RocketBody extends Item {
+
+    public RocketBody(){
+        super("Rocket Body", 'Ȫ');
+    }
+
+    public static RocketBody newRocketBodyInstance() {
+
+        RocketBody newRocketBody = new RocketBody();
+        newRocketBody.allowableActions.clear();
+        newRocketBody.allowableActions.add(new DropItemAction(newRocketBody));
+        return newRocketBody;
+
+    }
+
+
 }
