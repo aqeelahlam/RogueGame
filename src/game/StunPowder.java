346d92c01bb20829b5059c6789d203fa72343254
ygan0007 <ygan0007@student.monash.edu>
2019-05-10 01:53:13 +0800

    Updated StunPowder class

--- src/game/StunPowder.java
+++ src/game/StunPowder.java
@@ -1,4 +1,21 @@
 package game;
 
-public class StunPowder {
+import edu.monash.fit2099.engine.DropItemAction;
+import edu.monash.fit2099.engine.WeaponItem;
+
+public class StunPowder extends WeaponItem {
+
+    public StunPowder(){
+        super("Stun Powder", 'Ø',0,"stuns");
+    }
+
+    public static StunPowder newInventoryStunPowderInstance() {
+
+        StunPowder stunPowder = new StunPowder();
+        stunPowder.allowableActions.clear();
+        stunPowder.allowableActions.add(new DropItemAction(stunPowder));
+
+        return stunPowder;
+    }
+
 }
