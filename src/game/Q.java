d540070cf4107701fb83727fad333a3ab3f23c55
Aqeel Ahlam <aahl0002@student.monash.edu>
2019-05-10 07:54:05 +0800

    Completed: Q

--- src/game/Q.java
+++ src/game/Q.java
@@ -5,49 +5,60 @@ import edu.monash.fit2099.engine.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class is for Q who is a NPC(Non-Player-Character) in the game, he will give the player the RocketBody if
+ * the player has the RocketPlans with him.
+ */
 public class Q extends Actor {
 
-//    For Javadoc: Type "/**" and press enter
+    /**
+     * This is the constructor for Q
+     * This character is represented by 'Q' in the map.
+     * His priority is Second to the player
+     */
+    public Q(){
+        super("Q", 'Q',2,1000);
+    }
 
     /**
+     * This is an Overridden method of Actor:
+     * This method is used to give Q his possible Actions which is either talkBehavior or GivePlansAction
+     * which is usually controlled by the other Actor
+     * This will also check for RocketPlans within the players inventory and give the player the RocketBody.
      *
-     * @param name
-     * @param player
+     * @param otherActor the Actor that might be performing attack
+     * @param direction  String representing the direction of the other Actor
+     * @param map        current GameMap
+     * @return An array of allowable Actions
      */
-    public Q(String name, Actor player){
-        super(name, 'Q',2,300);
-
-    }
-    private List<ActionFactory> actionFactories = new ArrayList<ActionFactory>();
-
 
     @Override
     public Actions getAllowableActions(Actor otherActor, String direction, GameMap map) {
         Actions list = super.getAllowableActions(otherActor, direction, map);
+//        Iterate through the inventory of the otherActor
         for(Item item : otherActor.getInventory()){
             if (item.getDisplayChar() == 'Ø'){
-                list.add(new talkBehavior(otherActor, this, "Hand them over, I don't have all day!"));
+                list.add(new talkBehavior(otherActor, this, "Hand them over, I don’t have all day!"));
                 list.add(new GivePlansAction(item, RocketBody.newRocketBodyInstance(), otherActor, this));
                 return list;
             }
         }
 
-        list.add(new talkBehavior(otherActor,this,"I can give you something that will help, but I'm going to need the plans."));
-
+        list.add(new talkBehavior(otherActor,this,"I can give you something that will help, but I’m going to need the plans."));
         return list;
 
     }
 
-
-//    Do I really need this? CHECK
+    /**
+     * This is used to remove AttackAction and PickUpItemAction to prevent Q from harming the player or
+     * picking up an item in the map.
+     * @param actions collection of possible Actions for this Actor
+     * @param map     the map containing the Actor
+     * @param display the I/O object to which messages may be written
+     * @return
+     */
     @Override
     public Action playTurn(Actions actions, GameMap map, Display display) {
-        for (ActionFactory factory : actionFactories) {
-            Action action = factory.getAction(this, map);
-            if(action != null)
-                return action;
-        }
-
         for(Action action : actions){
             if((action instanceof AttackAction) || (action instanceof PickUpItemAction)){
                 actions.remove(action);
@@ -55,6 +66,5 @@ public class Q extends Actor {
         }
         return super.playTurn(actions,  map,  display);
     }
-
 
 }
