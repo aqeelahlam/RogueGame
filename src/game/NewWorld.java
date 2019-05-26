6c84a842e3b5c46033b01bd0d1b9bb3d46281d29
Aqeel Ahlam <aahl0002@student.monash.edu>
2019-05-27 04:14:41 +0800

    New: NewWorld: class to handle the game ending

---
+++ src/game/NewWorld.java
@@ -0,0 +1,13 @@
+package game;
+
+import edu.monash.fit2099.engine.Display;
+import edu.monash.fit2099.engine.World;
+
+public class NewWorld extends World {
+
+    public NewWorld(Display display){
+
+        super(display);
+
+    }
+}
